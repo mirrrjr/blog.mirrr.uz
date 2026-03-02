10eec890e234f97085b45a876031bed716966758
Mirsolih Mirzaahmad <mirrrrjr@gmail.com>
2026-03-02 10:36:39 +0500

    update deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+const isCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
+
 const nextConfig = {
-  // basePath: '/blog',
-  // assetPrefix: '/blog/',
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-    domains: ['*']
-  },
+    output: 'export',
+    basePath: isCustomDomain ? '' : '/blog',
+    assetPrefix: isCustomDomain ? '' : '/blog',
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+        domains: ['*']
+    },
 }
 
 export default nextConfig
